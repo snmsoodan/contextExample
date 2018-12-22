04f9f575c4934d7ffd9cee31787f3541a166a524
sanamdeep singh <sanamsoodan@gmail.com>
2018-12-22 17:23:34 -0500

    context with App containing flags

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import LanguageContext from '../contexts/LanguageContext'
+import ColorContext from '../contexts/ColorContext';
 
 class Button extends React.Component {
 
-    render() {
-        return(
-            <button className='ui button primary'>
+    renderButton(color) {
+        return (
+            <button className={`ui button ${color}`}>
                 <LanguageContext.Consumer>
-                    {(value) => value === 'english' ? 'Submit' : 'Voorlegen' }
+                        {(value) => value === 'english' ? 'Submit' : 'Voorlegen' }
                 </LanguageContext.Consumer>
             </button>
+        )
+    }
+
+    render() {
+        return(
+            <ColorContext.Consumer>
+                { color => this.renderButton(color) }
+            </ColorContext.Consumer>
         )
     }
 }
